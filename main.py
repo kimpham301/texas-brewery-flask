294f788a8f2c96d0f1afb598b6f74e1e41eb8c71
kimpham301 <ptp23@txstate.edu>
2022-04-26 15:38:44 -0500

    fetch multiple pages

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import requests
 import pymysql
+import json
+import threading
 from pymysql.cursors import DictCursor
 from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-r = requests.get('https://api.openbrewerydb.org/breweries?by_state=texas&per_page=50&page={}')
-package_json = r.json()
+response = list()
+page_num =1
+for page_num in range(10):
+    r = requests.get('https://api.openbrewerydb.org/breweries?by_state=texas&per_page=50&page={}'.format(page_num))
+    page_num = page_num +1;
+    data=json.loads(r.text)
+    response.append(data)
 
 con=pymysql.connect(
     host='localhost',
@@ -25,27 +32,27 @@ def validate_string(val):
         else:
             return val
 
-print(package_json[1])
 
-for i in package_json:
-    query='Insert ignore into new_table ('
-    t1=[]
-    first_item = True
-    for x in i:
-        xx=validate_string(i.get(x,None))
-        t1.append(xx)
-        if not first_item:
-            # add a comma and space to the query if it's not the first item
-            query += ', '
-        # add the field name to the query
-        query += x
-        # mark that it's no longer the first item
-        first_item = False
+for data in response:
+    for i in data:
+        query='Insert ignore into new_table ('
+        t1=[]
+        first_item = True
+        for x in i:
+            xx=validate_string(i.get(x,None))
+            t1.append(xx)
+            if not first_item:
+                # add a comma and space to the query if it's not the first item
+                query += ', '
+            # add the field name to the query
+            query += x
+            # mark that it's no longer the first item
+            first_item = False
 
-        # finish off the query string
-    query += ') VALUES {}'
-    # and send the query
-    cursor.execute(query.format(tuple(t1)))
+            # finish off the query string
+        query += ') VALUES {}'
+        # and send the query
+        cursor.execute(query.format(tuple(t1)))
 con.commit()
 con.close()
 
@@ -54,8 +61,10 @@ con.close()
 def get_data():  # put application's code here
     con.ping(reconnect=True)
     cursor.execute('SELECT * FROM new_table')
-    brewery_data = cursor.fetchall()
-    return jsonify(brewery_data)
+    brewery_data = jsonify(cursor.fetchall())
+    brewery_data.headers.add('Access-Control-Allow-Origin', '*')
+    return brewery_data
+
 
 
 
